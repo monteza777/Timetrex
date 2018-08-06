d8cd5c27e9dc0ef31aa99df1a2af47f9039cdd0f
monteza777 <montezamitch07@gmail.com>
2018-08-06 18:49:14 +0800

    insert is working

--- punches.php
+++ punches.php
@@ -1,42 +1,38 @@
+
 <?php
 include('conn.php');
-/*
-function get_users($conn) {
-	$result = pg_query($conn, "SELECT * FROM punch_control");
-	if (!$result) {
-	    echo "An error occurred.\n";
-	}	
-	echo 'good';
-	$users = pg_fetch_array($result);
-	return $users;
-}
-var_dump($users);
-*/
 
-$sql = "SELECT * FROM punch";
+$sql = "SELECT punch_control.user_id,punch.original_time_stamp,punch.status_id  FROM punch_control LEFT JOIN punch ON punch_control.id = punch.punch_control_id";
 $result = pg_query($conn, $sql);
 if (!$result) {
     die("Error in SQL query: " . pg_last_error());
 }
-/*
+
 $row = pg_fetch_all($result);
 //print_r($row);
 foreach($row as $p){
-	foreach($p as $key=>$value){
-	// $key.'='.$value . "<br>";
-	//var_dump($key);
-	echo $key.'='.$value.'<br>';
+        foreach($p as $key=>$value){
+        //var_dump($key);
+        //echo $key.'='.$value.'<br>';
+                if($key == 'user_id'){
+                //echo $key.'='.$value.'<br>';
+		$user_id = $value;
+                }
+		if($key =='original_time_stamp'){
+		$key.'='.$value.'<br>';
+		$time = $value;
+		$time_stamp =  date("Y-m-d H:i:s",strtotime($time));
+	//	echo $time_stamp.'<br>';	
+		}
+		if($key =='status_id'){
+		//echo $key.'='.$value.'<br>';
+		$status = $value;
+		}
 	}
+		$insert = " INSERT INTO hs_hr_timeattendance_log (user_id,state,time_log) VALUES ('$user_id','$status','$time_stamp')";
+		if($conn2->query($insert)){
+			echo $insert;
+		}
 }
-*/
-while($row = pg_fetch_array($result)){
-//while($row = pg_fetch_array($result)){
-	//echo $row[0].'<br>';
-	//var_dump($row);
-	foreach($row as $rows){
-	echo $rows.'<br>';
-	}
-//}
-//var_dump($row);
-}
+
 ?>
